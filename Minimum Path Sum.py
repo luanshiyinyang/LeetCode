bbaa51ec9d8c61b6a515b6c0328f78b2344c94f5
luanshiyinyang <1695735420@qq.com>
2019-04-18 16:28:51 +0800

    Minimum Path Sum

---
+++ Minimum Path Sum.py
@@ -0,0 +1,18 @@
+# -*-coding:utf-8-*-
+class Solution:
+    def minPathSum(self, grid) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0 for i in range(n)] for j in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+        return dp[-1][-1]
