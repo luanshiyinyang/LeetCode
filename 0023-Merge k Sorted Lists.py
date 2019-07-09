e566ef8b900b030dfee5ad2a0d97db0156c3d695
luanshiyinyang <1695735420@qq.com>
2019-07-09 18:03:35 +0800

    array

---
+++ 0023-Merge k Sorted Lists.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if len(lists) == 0:
+            return None
+        while(len(lists)>1):
+            lists.append(self.merge_two_lists(lists[0], lists[1]))
+            lists.pop(0)
+            lists.pop(0)
+        return lists[0]
+    def merge_two_lists(self, l1, l2):
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.merge_two_lists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.merge_two_lists(l1, l2.next)
+            return l2
