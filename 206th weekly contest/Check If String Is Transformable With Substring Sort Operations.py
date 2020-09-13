e18ecc392d5bd41ea2912047fde2761bf4cc2cfc
luanshiyinyang <luanshiyinyang@gmail.com>
2020-09-13 14:47:49 +0800

    206th weekly contest

--- 206th weekly contest/Check If String Is Transformable With Substring Sort Operations.py
+++ 206th weekly contest/Check If String Is Transformable With Substring Sort Operations.py
@@ -6,13 +6,16 @@ class Solution:
             idx[ord(c) - ord('0')].append(i)
         # 处理目标字符串
         for c in t:
+            print(c)
             index = ord(c) - ord('0')
             # 如果使用该字符已经达到了源串中该字符的数目，那么必然失败
             if use[index] >= len(idx[index]):
                 return False
-            # 检查是否有更小的字符在前面那么会阻止自由移动
+            # 检查是否有更小的字符在前面
             for i in range(index):
                 if use[i] < len(idx[i]) and idx[i][use[i]] < idx[index][use[index]]:
                     return False
             use[index] += 1
         return True
+
+Solution().isTransformable(s = "12345", t = "12435")
