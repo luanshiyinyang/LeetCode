a1491f09b077c1898aa3005b89a35c9bfbc02f21
luanshiyinyang <luanshiyinyang@gmail.com>
2022-02-07 12:58:06 +0800

    0050

---
+++ 0050-Pow(x, n).py
@@ -0,0 +1,11 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x, n = 1/x, -n
+        res = 1
+        while n:
+            if n&1:
+                res *= x
+            x *= x
+            n >>= 1
+        return res
